a479345adabd6aa018d04bae1efb2e60689526d1
Joacodef <joaquin.deferrari@sansano.usm.cl>
2023-01-13 00:39:13 -0300

    Manejo de excepciones, cambios en estructura y nombres, archivos base para nuevo modulo, generacion de ejecutable (en el txt).

---
+++ InvoiceExcel.py
@@ -0,0 +1,210 @@
+import pandas as pd
+import Employee as emp
+import Levenshtein
+import xlsxwriter
+
+excel_column_names = ["","A","B","C","D","E","F","G","H","I","J","K","L","M"]
+
+#filename = "TENS oct.xlsx"
+output_filename = "montosFacturas.xlsx"
+
+def extract_general_data(filename):
+    # Extract data from all the corresponding datasheets in the excel file
+    xlsx = pd.ExcelFile(filename)
+    sheet_num = len(xlsx.sheet_names)
+    sheet_names2 = []
+    dataFrames = []
+    for i in range(0,sheet_num):
+        # For each sheet, verify if it is a correct datasheet, and add it to the dataFrames list
+        df_1 = pd.read_excel(xlsx,sheet_name=i)
+        df_1.dropna(how='all', axis=1, inplace=True)
+        correct_procedure_col = False
+        correct_precio_col = False
+        for header in df_1.columns.values.tolist():
+            # Levenshtein distances allows us to compare how different 2 strings are. To account for typos or other errors, we search for a good enough coincidence
+            # The sheet must contain both the "procedimiento" and the "precio" columns or neither of them
+            # If the sheet contains only one of them, then raise an exception
+            if Levenshtein.distance("procedimiento",header.lower().strip())<3:
+                correct_procedure_col = True
+            if Levenshtein.distance("precio",header.lower().strip())<3: 
+                correct_precio_col = True
+        if correct_procedure_col and correct_precio_col:
+            dataFrames.append(df_1)
+            sheet_names2.append(xlsx.sheet_names[i])
+        elif correct_procedure_col and not correct_precio_col:
+            raise Exception("No se encontró columna \"precio\" en la hoja "+xlsx.sheet_names[i])
+        elif not correct_procedure_col and correct_precio_col:
+            raise Exception("No se encontró columna \"procedimiento\" en la hoja "+xlsx.sheet_names[i])
+    return dataFrames, sheet_names2
+
+def extract_employee_data(dataFrames, sheet_names):
+    employees = []
+    df_counter = 0 # Needed to obtain the corresponding sheet_name of the current dataframe
+    for df in dataFrames: # Iterate over the data of each dataFrame
+        rows = len(df.axes[0]) # Not used for now
+        cols = len(df.axes[1])
+        ops = df.iloc[:, 0] # It is assumed that each odd element will be NaN
+        for i in range(2,cols): # Iterate over employees in the dataframe
+            column = df.iloc[:, i] # Obtain each entire column in the dataframe
+            emp_found = False # Needed to indicate whether the employee already exists, and not add it again to the employees list
+            if 'Unnamed' in column.name: # Skip unnamed columns
+                continue
+            else:
+                # Search (by full name) if the employee has already been added to the list:
+                for em in employees:
+                    # Find the employee that has been added to the list:
+                    if Levenshtein.distance(em.name.upper().strip(),column.name.upper().strip()) < 2: # Account for possible typos or accent mark inconsistencies
+                        employee_aux = em
+                        emp_found = True
+                        break
+                if not emp_found:
+                    # If it hasn't been added, create a new object that represents that employee
+                    employee_aux = emp.Employee(column.name.upper().strip(), [])
+                j = 0
+                dict_job = dict() # Dictionary that will store all the jobs completed by the employee, and the money to be paid
+                for row in column:
+                    if j == len(column)-1: # Skip the last row, which contains the total amount to be paid to the employee
+                        continue
+                    if j % 2 == 0: # Even rows contain the number of procedures performed
+                        if isinstance(row, str):
+                            raise Exception("El valor de la columna de nombre "+column.name+", fila "+str(j+2)\
+                                +",\nen la hoja "+sheet_names[df_counter]+", no es un número.")
+                        if row > 0:
+                            dict_job[str(ops[j])] = [str(int(row))] # only add it if the amount of procedures performed is not zero
+                    else: # Odd rows contain the amount of money to be paid to the employee related to those specific procedures
+                        if isinstance(row, str):
+                            raise Exception("El valor de la columna de nombre "+column.name+", fila "+str(j+2)\
+                                +",\nen la hoja "+sheet_names[df_counter]+", no es un número.")
+                        if row > 0:
+                            if str(ops[j-1]) in dict_job:
+                                dict_job[str(ops[j-1])].append(int(row)) # The money to be paid should be higher than zero if procedures are not zero
+                    j += 1
+                dict_job["job_name"] = sheet_names[df_counter] # Add an element to the dictionary that specifies the type of job performed
+                employee_aux.jobs_list.append(dict_job)
+                if not emp_found:
+                    employees.append(employee_aux) 
+        df_counter += 1 
+    return employees
+
+# Generate the output excel based on the information of the input excel:
+def generate_emp_tables(employees):
+    try:
+        counter = 1 # Indicates the row in which the information has to be written
+        workbook = xlsxwriter.Workbook(output_filename) # Create a new excel file
+        worksheet = workbook.add_worksheet()
+        normal = workbook.add_format({'border': 1})
+        bold = workbook.add_format({'bold': True, 'border': 1})
+        final = workbook.add_format({'bold': True, 'border': 1})
+        final.set_bg_color("#8DB4E2")
+        money_format = workbook.add_format({'num_format': '$#,##0', 'border': 1})
+        money_format_bold = workbook.add_format({'num_format': '$#,##0', 'bold': True, 'border': 1})
+        money_format_final = workbook.add_format({'num_format': '$#,##0', 'bold': True, 'border': 1})
+        money_format_final.set_bg_color("#8DB4E2")
+        for i in range(0,8,2):
+            worksheet.set_column(i,i, 25)
+            worksheet.set_column(i+1,i+1, 12)
+    except Exception as e:
+        raise Exception("Error en la inicialización del excel de salida. "+str(e))
+
+
+    for emp in employees:
+        try:
+            job_num = 1 # Indicates the column in which the information has to be written
+            worksheet.write('A'+str(counter),emp.name) # Always starts in the column 'A'
+            counter += 1
+            max_procedures = 0
+            # First, iterate over each job, and write each procedure in the job, next to the money to be paid for it:
+            for job in emp.jobs_list:
+                # For each job, write the job name first, and then the procedures performed in that job:
+                merge_format = workbook.add_format({
+                    'bold': 1,
+                    'border': 1,
+                    'align': 'center',
+                    'valign': 'vcenter',
+                    'fg_color': 'white'})
+                worksheet.merge_range(excel_column_names[job_num]+str(counter)+":"+excel_column_names[job_num+1]+str(counter),job["job_name"], merge_format)
+                counter_aux = counter + 1 # An auxiliary counter has to be used, because we want to go back to the initial row after finishing each job specification
+                for procedure in job.keys():
+                    # For each procedure, write its name and the amount that has to be paid for it in the same row
+                    if Levenshtein.distance(str(procedure).upper().strip(),"JOB_NAME")>1:
+                        worksheet.write(excel_column_names[job_num]+str(counter_aux), str(job[procedure][0])+" "+str(procedure), normal)
+                        if len(job[procedure]) > 1:
+                            worksheet.write(excel_column_names[job_num+1]+str(counter_aux), job[procedure][1], money_format)
+                        else:
+                            worksheet.write(excel_column_names[job_num+1]+str(counter_aux), "", normal)
+                        counter_aux += 1
+                    if counter_aux > max_procedures:
+                        max_procedures = counter_aux # Keep track of the highest row in which information has been written
+                job_num += 2
+        except IndexError:
+                raise Exception("El empleado "+emp.name+" tiene demasiados trabajos asignados - "+str(int(job_num/2))+\
+                    ". Verificar que su nombre no se repita en más de una columna dentro de la misma hoja.")
+        except Exception as e:
+            raise Exception("Error al generar la tabla del empleado "+emp.name+". "+str(e)+".")
+
+
+        # Iterate again over jobs, to write the total amount of money for each one done, and the total to be paid:
+        job_num = 1
+        job_total = len(emp.jobs_list)
+        total_total = 0 # Total amount of money to be paid to the employee
+        
+        for job in emp.jobs_list:
+            try:
+                if job_total > 1:
+                    worksheet.write(excel_column_names[job_num]+str(max_procedures), "subtotal", normal)
+                    subtotal = 0.0 # Amount of money to be paid for each kind of job
+                    for procedure in job.keys():
+                        if Levenshtein.distance(str(procedure).upper().strip(),"JOB_NAME")>1:
+                            if len(job[procedure]) > 1:
+                                subtotal += float(job[procedure][1])
+                    worksheet.write(excel_column_names[job_num+1]+str(max_procedures), int(subtotal), money_format)
+                    job_num += 2
+                    total_total += subtotal
+                else:
+                    for procedure in job.keys():
+                        if Levenshtein.distance(str(procedure).upper().strip(),"JOB_NAME")>1:
+                            if len(job[procedure]) > 1:
+                                total_total += float(job[procedure][1])
+                    max_procedures-=1
+            except IndexError:
+                raise Exception("El empleado "+emp.name+" tiene demasiados trabajos asignados - "+str(int(job_num/2))+\
+                    ". Verificar que su nombre no se repita en demasiadas columnas.")
+            except:
+                raise Exception("Error al calcular el subtotal de la hoja "+job["job_name"]+" del empleado "+emp.name+".")
+        """
+        # In case we want to merge cells for the rows total, 4% and MONTO BOLETA:
+        
+        if job_total > 1:
+            worksheet.merge_range('A'+str(max_procedures+1)+":"+excel_column_names[int(job_total)]+str(max_procedures+1),"total", bold)
+            worksheet.merge_range(excel_column_names[int(job_total)+1]+str(max_procedures+1)+":"+excel_column_names[job_total*2]+\
+                                str(max_procedures+1),int(total_total), money_format_bold)
+        else:
+        """
+        worksheet.write('A'+str(max_procedures+1), "total", normal)
+        worksheet.write('B'+str(max_procedures+1), int(total_total), money_format)
+        worksheet.write('A'+str(max_procedures+2), "4%", bold)
+        worksheet.write('B'+str(max_procedures+2), int(total_total)*0.04, money_format_bold)
+        worksheet.write('A'+str(max_procedures+3), "MONTO BOLETA", final)
+        worksheet.write('B'+str(max_procedures+3), int(total_total)*0.96, money_format_final)
+        counter = max_procedures + 6
+    workbook.close()
+
+"""
+# Tests to check if the script is working correctly:
+dataFrames, sheet_names = extract_general_data(filename)
+employees = extract_employee_data(dataFrames, sheet_names)
+generate_emp_tables(employees)
+"""
+
+"""
+#print(dataFrames)
+#print(sheet_names)
+# Test if the script is able to correctly find name coincidences in different sheets:
+count = 0
+for employee in employees:
+    if len(employee.jobs_list) > 1:
+        print(employee.name)
+        print(len(employee.jobs_list))
+        count += 1
+print(count)
+"""
---
+++ InvoiceMain.py
@@ -0,0 +1,137 @@
+import InvoiceExcel
+import PySimpleGUI as sg
+import os
+import datetime
+import sys
+
+# Set the Desktop as the working directory:
+os.chdir(os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop'))
+working_directory = os.getcwd()
+
+LOGS = False
+input_text_key = "rutaArchivo"
+output_filename = "montosFacturas.xlsx"
+
+def displayMainWindow():
+    # Create main window:
+    layout = getLayout("selectTablaPagos")
+    window = sg.Window('Elegir archivo', layout, element_justification='c')
+     
+    while True:
+        # Wait to read the user's input:
+        event, values = window.read()
+        # If the user closes the window or clicks the "Cancel" button, then exit the program:
+        if event in [sg.WIN_CLOSED,'Cancelar']:
+            break
+        elif event in ['Seleccionar']:
+            # If the user clicks the "Seleccionar" button, 2 things must be checked:
+            # 1.- Check if there is already an output file in the same directory, and allow user to choose whether to replace it or not:
+            promptExcelOverwrite(output_filename)
+
+            # 2.- Once the user has selected a file, check if it has the correct file extension and if the older output file can be removed:
+            if ".xlsx" not in values[input_text_key]:
+                raiseException("1: El archivo seleccionado no tiene extensión \'.xlsx\'.")            
+
+            # If all the checks are passed, try to create the output file:
+            try:
+                createExcel(values["rutaArchivo"])
+                # Check if the output file was created successfully:
+                if os.path.exists(output_filename):
+                    layout2 = getLayout("creationSuccess")
+                else:
+                    # This would be an unknown error, so a generic message is displayed:
+                    layout2 = getLayout("creationUnknownFailure")
+                
+                # Display a window with the result of the operation (layout2):
+                window2 = sg.Window('Resultado', layout2, element_justification='c')
+                while True:
+                    event2, _ = window2.read()
+                    if event2 in [sg.WIN_CLOSED,'Aceptar']:
+                        break     
+            except Exception as error:
+                # If there was an error creating the output file, then display an error message:
+                raiseException(str(error))
+            break 
+            
+    window.close()
+
+def raiseException(error_str):
+    # A log file can be generated, though it is recommended to use it only when using the program in a production environment:
+    if LOGS:
+        f = open("errorLogs.txt", "a")
+        type, value, traceback = sys.exc_info()
+        f.write(str(datetime.datetime.now())+" "+str(type)+" "+str(value)+" "+str(traceback)+"\n")
+        f.close()
+    # Display an error window:
+    layout_err = getLayout("creationFailure", error_str)
+    window_err = sg.Window('Resultado', layout_err, element_justification='c')
+    while True:
+        event_err, _ = window_err.read()
+        if event_err == 'Aceptar':
+            # The program is always closed after an error:
+            window_err.close()
+            exit()
+
+def promptExcelOverwrite(filename):
+    # Display a window to ask the user if they want to replace the output file:
+    if os.path.exists(filename):
+        layout_prompt = [[sg.Text("Atención: Ya existe un archivo excel \""+output_filename+"\" en el escritorio. ¿Desea reemplazarlo?")],[sg.Button("Aceptar"),sg.Button("Cancelar")]]
+        window_prompt = sg.Window('Resultado', layout_prompt, element_justification='c')
+        while True:
+            event_prompt, _ = window_prompt.read()
+            if event_prompt == 'Aceptar':
+                try:
+                    os.remove(output_filename)
+                except Exception:
+                    raiseException("El archivo \""+output_filename+"\" está abierto y no puede ser eliminado.")
+                window_prompt.close()
+                break
+            else:
+                exit()
+
+def createExcel(filename):
+    # Call all the functions to create the output file:
+    try:
+        dataFrames, sheet_names = InvoiceExcel.extract_general_data(filename)
+        employees = InvoiceExcel.extract_employee_data(dataFrames, sheet_names)
+        InvoiceExcel.generate_emp_tables(employees)
+    except Exception as e:
+        print(e)
+        raise
+
+def getLayout(layout_id, error_str=""):
+    # This function returns the layout of the window depending on the layout_id:
+    if layout_id == "selectTablaPagos":
+        layout = [
+            [sg.Text("Seleccione Archivo: ")],
+            [sg.InputText(key=input_text_key),
+            sg.FileBrowse(initial_folder=working_directory)],
+            [sg.Button("Seleccionar"), sg.Button("Cancelar")]
+        ]
+    elif layout_id == "creationSuccess":
+        layout = [
+            [sg.Text("Se ha generado un nuevo archivo \""+output_filename+"\" en el escritorio.")],
+            [sg.Button("Aceptar")]
+        ]
+    elif layout_id == "creationUnknownFailure":
+        layout = [
+            [sg.Text("0: Error en la creacion del archivo excel. Por favor, inténtelo de nuevo.")],
+            [sg.Button("Aceptar")]
+        ]
+    elif layout_id == "creationFailure":
+        layout = [
+            [sg.Text("No se ha podido completar la operación. El error es:")],
+            [sg.Text(str(error_str),justification='center', font=("Arial",11,'italic'))],
+            [sg.Text("Por favor, inténtelo de nuevo.")],[sg.Button("Aceptar")]
+        ]
+    return layout
+
+displayMainWindow()
+
+"""
+# Specify the "current working directory" as the directory where this script is located:
+full_script_route = os.path.realpath(__file__)
+pos_scriptname = full_script_route.find(os.path.basename(__file__))
+script_folder = full_script_route[:pos_scriptname]
+os.chdir(script_folder)
+"""
